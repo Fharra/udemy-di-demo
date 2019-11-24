cb58474eee466f1b32605030500edccfc9f488a2
Fharra <fharra1@hotmail.com>
2019-11-24 20:50:12 +0100

    dependency injection

---
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -0,0 +1,23 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.GreetingService;
+import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+@Controller
+public class ConstructorInjectedController {
+    // no need @Autowired, automatically wired from constructor
+    private GreetingService greetingService;
+
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
