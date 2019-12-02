3dc94defb503d4b1a8910e4b9de6d40b500d9bce
Fharra <fharra1@hotmail.com>
2019-12-02 13:55:13 +0100

    Override properties

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -4,6 +4,7 @@ import guru.springframework.controllers.ConstructorInjectedController;
 import guru.springframework.controllers.MyController;
 import guru.springframework.controllers.PropertyInjectedController;
 import guru.springframework.controllers.SetterInjectedController;
+import guru.springframework.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,10 +15,9 @@ public class DiDemoApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 		MyController controller = (MyController) ctx.getBean("myController");
-		//controller.hello();
-		System.out.println(controller.hello());
-		System.out.println((ctx.getBean(PropertyInjectedController.class)).sayHello());
-		System.out.println((ctx.getBean(SetterInjectedController.class)).sayHello());
-		System.out.println((ctx.getBean(ConstructorInjectedController.class)).sayHello());
+		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
+		System.out.println(fakeDataSource.getPassword());
+		System.out.println(fakeDataSource.getUrl());
 	}
 }
